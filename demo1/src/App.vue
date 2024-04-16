9031ac586b7d77dd5a2b7ef7d923d92eb95b296e
Chao <lwc19990611@outlook.com>
2024-04-17 01:13:27 +0800

    format

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-
-import BabylonCore from './components/corecomponents/appcore.vue';
+import BabylonCore from "./components/corecomponents/appcore.vue";
 </script>
 
 <template>
   <BabylonCore></BabylonCore>
-
-
 </template>
--- demo1/src/components/corecomponents/appcore.vue
+++ demo1/src/components/corecomponents/appcore.vue
@@ -1,27 +1,25 @@
 <template>
-    <canvas ref="webglRef">
-
-    </canvas>
+  <canvas ref="webglRef"> </canvas>
 </template>
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue';
-import AppCore from './appcore';
+import { onMounted, onUnmounted, ref } from "vue";
+import AppCore from "./appcore";
 
-const webglRef=ref<HTMLCanvasElement | null>(null);
+const webglRef = ref<HTMLCanvasElement | null>(null);
 
-let appCore:AppCore;
+let appCore: AppCore;
 
-function windowResize(){
-   appCore.engine.resize();
+function windowResize() {
+  appCore.engine.resize();
 }
 
-onMounted(()=>{
-    if(webglRef.value){
-        appCore= new AppCore(webglRef.value);
-        window.addEventListener('resize',windowResize);
-    }
-})
-onUnmounted(()=>{
-    window.removeEventListener('resize',windowResize);
-})
+onMounted(() => {
+  if (webglRef.value) {
+    appCore = new AppCore(webglRef.value);
+    window.addEventListener("resize", windowResize);
+  }
+});
+onUnmounted(() => {
+  window.removeEventListener("resize", windowResize);
+});
 </script>
