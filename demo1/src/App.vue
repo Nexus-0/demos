887552f40e1a9b705ebf093c303ec77626002ca7
Nexus-0 <72970448+Nexus-0@users.noreply.github.com>
2024-04-21 20:57:43 +0800

    change appcore

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import BabylonCore from "./components/corecomponents/appcore.vue";
-import appUI from "./components/appUI.vue";
+
 </script>
 
 <template>
   <BabylonCore></BabylonCore>
-  <appUI></appUI>
+
 </template>
